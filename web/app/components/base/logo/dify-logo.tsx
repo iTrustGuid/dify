f4fd40c26dee5152dc3f46b6f6b56b6ee105fb89
李告 <ligao@esconsoft.com>
2026-01-27 11:15:49 +0800

    fix: 修改网站标题与logo

--- web/app/components/base/logo/dify-logo.tsx
+++ web/app/components/base/logo/dify-logo.tsx
@@ -6,8 +6,8 @@ import { basePath } from '@/utils/var'
 export type LogoStyle = 'default' | 'monochromeWhite'
 
 export const logoPathMap: Record<LogoStyle, string> = {
-  default: '/logo/logo.svg',
-  monochromeWhite: '/logo/logo-monochrome-white.svg',
+  default: '/logo/icon-wn.png',
+  monochromeWhite: '/logo/icon-wn.png',
 }
 
 export type LogoSize = 'large' | 'medium' | 'small'
--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -77,7 +77,7 @@ const LocaleLayout = async ({
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="apple-mobile-web-app-title" content="Dify" />
+        <meta name="apple-mobile-web-app-title" content="万年县不动产登记快易办" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/icon-192x192.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/icon-192x192.png" />
